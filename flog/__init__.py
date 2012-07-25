d6f7d062cecba38f0d8a67223749e1c438455740
Jeremy Hughes <jedahu@gmail.com>
2012-07-26 04:54:51 +1200

    add flog:latest-posts macro to asciidoc

--- flog/__init__.py
+++ flog/__init__.py
@@ -50,23 +50,6 @@ DEBUG = __name__ == '__main__' and 'nodebug' not in sys.argv
 app.static_folder = join(THEME_PATH, 'static')
 app.template_folder = THEME_PATH
 
-sys.path.append(join(THIS_DIR, 'asciidoc'))
-from asciidocapi import AsciiDocAPI
-ASCIIDOC_PY = join(THIS_DIR, 'asciidoc', 'asciidoc.py')
-def asciidoc():
-  '''Return AsciiDocAPI object configured for flog'''
-  ad = AsciiDocAPI(asciidoc_py=ASCIIDOC_PY)
-  ad.options('--no-header-footer')
-  ad.attributes['flog-posts-path'] = POSTS_PATH
-  ad.attributes['flog-pages-path'] = PAGES_PATH
-  ad.attributes['pygments'] = 'pygments'
-  ad.attributes['conf-files'] = ASCIIDOC_CONF
-  if ASCIIDOC_USER_CONF and ASCIIDOC_USER_CONF.trim() != '':
-    ad.attributes['conf-files'] += '|' + ASCIIDOC_USER_CONF.trim()
-  return ad
-ASCIIDOC = asciidoc()
-
-
 # Config checks
 if not ROOT_URL:
   raise Exception('ROOT_URL not set')
@@ -98,97 +81,6 @@ else:
   def cache():
     return identity
 
-
-# Routes
-@app.route('/')
-def root():
-  '''Site index'''
-  @cache()
-  def root_impl():
-    posts = os.listdir(join(SRC_DIR, POSTS_PATH))
-    posts.sort(key=int, reverse=True)
-    posts = [(n, post_meta(n)) for n in posts[:FEED_SIZE]]
-    content = asciidoc_html(join(SRC_DIR, PAGES_PATH, 'index'), '')
-    return render_template('index.html',
-        posts=posts,
-        content=content)
-  return root_impl()
-
-@app.route(join('/', POSTS_PATH, '<int:n>') + '/')
-def post(n):
-  '''Blog post'''
-  @cache()
-  def post_impl(n):
-    content, meta = parse_post(n, join('/', POSTS_PATH, str(n)))
-    prev_meta = None
-    next_meta = None
-    if post_exists(n - 1):
-      prev_meta = post_meta(n - 1)
-    if post_exists(n + 1):
-      next_meta = post_meta(n + 1)
-    return render_template('post.html',
-        n=n,
-        meta=meta,
-        prev_meta=prev_meta,
-        next_meta=next_meta,
-        content=content)
-  return post_impl(n)
-
-@app.route(join('/', POSTS_PATH) + '/')
-def posts_index():
-  '''Blog post index'''
-  @cache()
-  def posts_index_impl():
-    posts = os.listdir(join(SRC_DIR, POSTS_PATH))
-    posts.sort(key=int, reverse=True)
-    def get_data(n):
-      n = int(n)
-      content, meta = parse_post(n, join('/', POSTS_PATH, str(n)))
-      return n, content, meta
-    posts = [get_data(n) for n in posts[:FEED_SIZE]]
-    last, _, _ = posts[-1]
-    prev_meta = None
-    if post_exists(last - 1):
-      prev_meta = post_meta(last - 1)
-    return render_template('posts_index.html',
-        posts=posts,
-        n=last,
-        prev_meta=prev_meta)
-  return posts_index_impl()
-
-
-@app.route(normpath(join('/', PAGES_PATH) + '/<path:path>'))
-def page_or_media(path):
-  '''Asciidoc page or other (media) file'''
-  fpath = join(SRC_DIR, PAGES_PATH, path)
-  if path.endswith('/'):
-    return page(fpath, '/' + path[:-1])
-  else:
-    if isfile(fpath):
-      return media(fpath)
-    elif isdir(fpath):
-      return redirect(url_for('page_or_media', path = path + '/'))
-    else:
-      abort(404)
-
-@app.route('/favicon.ico')
-@app.route('/favicon.ico/')
-def favicon():
-  abort(404)
-
-@app.route(join('/', POSTS_PATH, 'feed') + '/')
-def posts_feed():
-  '''Blog posts atom feed'''
-  @cache()
-  def posts_feed_impl():
-    return generate_feed()
-  return posts_feed_impl()
-
-
-@app.context_processor
-def inject_template_vars():
-  '''Make these vars available to all templates'''
-  return dict(len=len)
 
 
 # Helper functions
@@ -283,10 +175,7 @@ def asciidoc_meta(fpath, abs_url):
         break
       elif meta_match:
         name = meta_match.group(1).lower()
-        if name == 'summary':
-          value = asciidoc_html_from_string(meta_match.group(2), abs_url)
-        else:
-          value = meta_match.group(2)
+        value = meta_match.group(2)
         meta[name] = value
       elif author_match:
         author = author_match.group(1)
@@ -331,5 +220,123 @@ def generate_feed():
   return feed.get_response()
 
 
+
+
+# AsciiDocAPI
+sys.path.append(join(THIS_DIR, 'asciidoc'))
+from asciidocapi import AsciiDocAPI
+ASCIIDOC_PY = join(THIS_DIR, 'asciidoc', 'asciidoc.py')
+def asciidoc():
+  '''Return AsciiDocAPI object configured for flog'''
+  def latest_post_titles():
+    posts = os.listdir(join(SRC_DIR, POSTS_PATH))
+    posts.sort(key=int, reverse=True)
+    posts = [(n, post_meta(n)) for n in posts[:FEED_SIZE]]
+    return app.jinja_env.get_or_select_template('latest_post_titles.html')\
+        .render(dict(POSTS_PATH=POSTS_PATH, posts=posts))
+  ad = AsciiDocAPI(asciidoc_py=ASCIIDOC_PY)
+  ad.options('--no-header-footer')
+  ad.attributes['flog-posts-path'] = POSTS_PATH
+  ad.attributes['flog-pages-path'] = PAGES_PATH
+  ad.attributes['flog-latest-post-titles'] = latest_post_titles()
+  ad.attributes['pygments'] = 'pygments'
+  ad.attributes['conf-files'] = ASCIIDOC_CONF
+  if ASCIIDOC_USER_CONF and ASCIIDOC_USER_CONF.trim() != '':
+    ad.attributes['conf-files'] += '|' + ASCIIDOC_USER_CONF.trim()
+  return ad
+
+ASCIIDOC = asciidoc()
+
+
+# Routes
+@app.route('/')
+def root():
+  '''Site index'''
+  @cache()
+  def root_impl():
+    posts = os.listdir(join(SRC_DIR, POSTS_PATH))
+    posts.sort(key=int, reverse=True)
+    posts = [(n, post_meta(n)) for n in posts[:FEED_SIZE]]
+    content = asciidoc_html(join(SRC_DIR, PAGES_PATH, 'index'), '')
+    return render_template('index.html',
+        posts=posts,
+        content=content)
+  return root_impl()
+
+@app.route(join('/', POSTS_PATH, '<int:n>') + '/')
+def post(n):
+  '''Blog post'''
+  @cache()
+  def post_impl(n):
+    content, meta = parse_post(n, join('/', POSTS_PATH, str(n)))
+    prev_meta = None
+    next_meta = None
+    if post_exists(n - 1):
+      prev_meta = post_meta(n - 1)
+    if post_exists(n + 1):
+      next_meta = post_meta(n + 1)
+    return render_template('post.html',
+        n=n,
+        meta=meta,
+        prev_meta=prev_meta,
+        next_meta=next_meta,
+        content=content)
+  return post_impl(n)
+
+@app.route(join('/', POSTS_PATH) + '/')
+def posts_index():
+  '''Blog post index'''
+  @cache()
+  def posts_index_impl():
+    posts = os.listdir(join(SRC_DIR, POSTS_PATH))
+    posts.sort(key=int, reverse=True)
+    def get_data(n):
+      n = int(n)
+      content, meta = parse_post(n, join('/', POSTS_PATH, str(n)))
+      return n, content, meta
+    posts = [get_data(n) for n in posts[:FEED_SIZE]]
+    last, _, _ = posts[-1]
+    prev_meta = None
+    if post_exists(last - 1):
+      prev_meta = post_meta(last - 1)
+    return render_template('posts_index.html',
+        posts=posts,
+        n=last,
+        prev_meta=prev_meta)
+  return posts_index_impl()
+
+
+@app.route(normpath(join('/', PAGES_PATH) + '/<path:path>'))
+def page_or_media(path):
+  '''Asciidoc page or other (media) file'''
+  fpath = join(SRC_DIR, PAGES_PATH, path)
+  if path.endswith('/'):
+    return page(fpath, '/' + path[:-1])
+  else:
+    if isfile(fpath):
+      return media(fpath)
+    elif isdir(fpath):
+      return redirect(url_for('page_or_media', path = path + '/'))
+    else:
+      abort(404)
+
+@app.route('/favicon.ico')
+@app.route('/favicon.ico/')
+def favicon():
+  abort(404)
+
+@app.route(join('/', POSTS_PATH, 'feed') + '/')
+def posts_feed():
+  '''Blog posts atom feed'''
+  @cache()
+  def posts_feed_impl():
+    return generate_feed()
+  return posts_feed_impl()
+
+
+@app.context_processor
+def inject_template_vars():
+  '''Make these vars available to all templates'''
+  return dict(len=len)
 if __name__ == '__main__':
   app.run(debug=True)

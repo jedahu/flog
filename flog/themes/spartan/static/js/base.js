36424c0571cbf77b0b0b3e4d53c8089d62e230ed
Jeremy Hughes <jedahu@gmail.com>
2012-08-17 04:42:56 +1200

    spartan theme: facebook and twitter integration

--- flog/themes/spartan/static/js/base.js
+++ flog/themes/spartan/static/js/base.js
@@ -1,1 +1,17 @@
 jQuery.noConflict();
+
+// Comments toggle
+jQuery(function($) {
+  var comments = $('.comments');
+  $('.comments-toggle').click(function() {
+    comments.slideToggle();
+  });
+});
+
+// Twitter
+jQuery(function($) {
+  var js = document.createElement('script');
+  js.id = 'twitter-wjs';
+  js.src = '//platform.twitter.com/widgets.js';
+  $(document.head).append(js);
+});

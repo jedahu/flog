91ce73fb913977605be23b3efe5023ebc37b37d4
Jeremy Hughes <jedahu@gmail.com>
2012-08-30 22:49:47 +1200

    config.py: update names and defaults

--- flog/config.py
+++ flog/config.py
@@ -3,7 +3,6 @@ import flask.config
 import flog.source
 
 CONFIG_DEFAULTS = dict(
-    SRC_DIR = '.',
     POSTS_PATH = 'posts',
     THEME_PATH = 'spartan',
     FEED_SIZE = 10,
@@ -16,6 +15,7 @@ CONFIG_DEFAULTS = dict(
     CACHE_PATH = '/tmp/flog-cache',
     CACHE_EXPIRE = 300,
     INDEX_NAME = 'index',
+    NAV = [],
     PLUGINS = {}
     )
 
@@ -27,7 +27,7 @@ class Config(flask.config.Config):
     self.from_pyfile(self.FLOG_CONF)
 
     self['FLOG_DIR'] = os.path.dirname(self.FLOG_CONF)
-    self['SOURCE_URL'] = os.environ.get('SOURCE_URL') or self.SOURCE_URL
+    self['SOURCE_URL'] = os.environ.get('FLOG_SOURCE_URL') or self.SOURCE_URL
     theme_path = self.THEME_PATH
     self['THEME_PATH'] = os.path.join(self.FLOG_DIR, theme_path)
     if not os.path.isdir(self.THEME_PATH):
@@ -36,7 +36,7 @@ class Config(flask.config.Config):
       self['FEED_URL'] = self.ROOT_URL + '/' + self.POSTS_PATH + '/feed/'
     self['ASCIIDOC_FLOG_CONF'] = os.path.join(self.root_path, 'asciidoc-html5.conf')
 
-    self['CACHE_PATH'] = os.environ.get('FLOG_CACHE') or self.CACHE_PATH
+    self['CACHE_PATH'] = os.environ.get('FLOG_CACHE_PATH') or self.CACHE_PATH
     self['CACHE_EXPIRE'] = os.environ.get('FLOG_CACHE_EXPIRE') or self.CACHE_EXPIRE
 
     self['SOURCE'] = flog.source.Source(self.CACHE_PATH, self.CACHE_EXPIRE)
